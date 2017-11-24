a8f1608d83e9f046c4bb2db074b8e0a871852139
toChaim <toChaim@gmail.com>
2017-11-24 02:28:43 -0800

    too many changes.

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-exports.MONGO_DB = process.env.MONGODB_URI || 'mongodb://localhost/CBT-API';
+exports.MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/GRATBOT';
 exports.MONGO_DEBUG = process.env.ENV === 'devolopment';
 
 exports.PORT = process.env.PORT || 8000;
