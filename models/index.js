bf47a5777c784a1e859e58f6d8bea7b61ceeea78
toChaim <toChaim@gmail.com>
2017-11-24 02:28:43 -0800

    too many changes.

--- models/index.js
+++ models/index.js
@@ -1,2 +1,17 @@
 const mongoose = require('mongoose');
-const { MONGO_DEBUG } = require('../config');
+
+mongoose.set('debug', process.env.ENV === 'devolopment');
+mongoose.Promise = Promise;
+
+mongoose
+  .connect(process.env.MONGODB_URI || 'mongodb://localhost/GRATBOT')
+  .then(ret => {
+    console.log('Database Connected: ', ret);
+  })
+  .catch(err => {
+    if (process.env.ENV === 'devolopment') {
+      console.log('Database Connection Failure: ', err);
+    }
+  });
+
+module.exports.User = require('./user');
