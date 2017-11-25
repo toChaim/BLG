a5762e5f8a9ba1799f4974d8937469eb9ec5b096
toChaim <toChaim@gmail.com>
2017-11-25 12:44:04 -0800

    clean out and change message.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # gratbot
-A gratitude chat bot.
+A game companie dedicated to make life better for our players, our programers, and people who live near them.
--- config.js
+++
@@ -1,4 +0,0 @@
-exports.MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/GRATBOT';
-exports.MONGO_DEBUG = process.env.ENV === 'devolopment';
-
-exports.PORT = process.env.PORT || 8000;
