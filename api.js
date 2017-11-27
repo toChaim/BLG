da8cdd22dcb62fb93abf487327c18aa36a29f93d
toChaim <toChaim@gmail.com>
2017-11-27 15:12:35 -0800

    add error routes.

--- api.js
+++ api.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-const { PORT } = require('./config');
+const { PORT, DEVELOPMENT } = require('./config');
 const userRoutes = require('./routes/users');
 const gameRoutes = require('./routes/games');
 
@@ -11,41 +11,6 @@ const api = express();
 api.use(morgan('tiny'));
 api.use(bodyParser.urlencoded({ extended: true }));
 api.use(bodyParser.json());
-
-// connect database
-// mongoose.set('debug', true);
-// mongoose.connect('mongodb://localhost/BLG');
-// mongoose.Promise = Promise;
-
-// const userSchema = new mongoose.Schema({
-//   userName: String,
-//   password: String
-// });
-
-// userSchema.pre('save', function(next) {
-//   var user = this;
-//   if (!user.isModified('password')) return next();
-//   bcrypt.hash(user.password, 14).then(
-//     hashedPassword => {
-//       user.password = hashedPassword;
-//       next();
-//     },
-//     err => next(err)
-//   );
-// });
-
-// userSchema.methods.comparePassword = function(candidatePassword, next) {
-//   bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
-//     if (err) return next(err);
-//     next(null, isMatch);
-//   });
-// };
-
-// set up mongoose model
-// const User = mongoose.model('User', userSchema);
-
-// config constants
-// const SECRET_KEY = 'TOP SECRET KEY';
 
 // routes
 // api.post('/signup', (req, res) => {
@@ -97,32 +62,32 @@ api.use(bodyParser.json());
 //   }
 // });
 
-// api.use((req, res, next) => {
-//   var err = new Error('Not Found');
-//   err.status = 404;
-//   next(err);
-// });
-
-// if (api.get('env') === 'development') {
-//   api.use((err, req, res, next) => {
-//     res.status(err.status || 500);
-//     res.send({
-//       message: err.message,
-//       error: err
-//     });
-//   });
-// }
-
-// api.use((err, req, res, next) => {
-//   res.status(err.status || 500);
-//   res.send({
-//     message: err.message,
-//     error: {}
-//   });
-// });
-
 api.use('/users', userRoutes);
 api.use('/', gameRoutes);
+
+api.use((req, res, next) => {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+if (DEVELOPMENT) {
+  api.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.send({
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+api.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.send({
+    message: err.message,
+    error: {}
+  });
+});
 
 api.listen(PORT, () => {
   console.log('Server is listening on port 3000.');
--- models/user.js
+++ models/user.js
@@ -17,5 +17,24 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
+// userSchema.pre('save', function(next) {
+//   var user = this;
+//   if (!user.isModified('password')) return next();
+//   bcrypt.hash(user.password, 14).then(
+//     hashedPassword => {
+//       user.password = hashedPassword;
+//       next();
+//     },
+//     err => next(err)
+//   );
+// });
+
+// userSchema.methods.comparePassword = function(candidatePassword, next) {
+//   bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+//     if (err) return next(err);
+//     next(null, isMatch);
+//   });
+// };
+
 const User = mongoose.model('User', userSchema);
 module.exports = User;
