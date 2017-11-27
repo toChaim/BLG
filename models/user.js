3a24afedc80849ecf72ea60a924e3eeef03b9e10
toChaim <toChaim@gmail.com>
2017-11-27 15:26:04 -0800

    start on auth.

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
-// const jwt = require('jsonwebtoken');
-// const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema(
@@ -17,24 +17,24 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-// userSchema.pre('save', function(next) {
-//   var user = this;
-//   if (!user.isModified('password')) return next();
-//   bcrypt.hash(user.password, 14).then(
-//     hashedPassword => {
-//       user.password = hashedPassword;
-//       next();
-//     },
-//     err => next(err)
-//   );
-// });
+userSchema.pre('save', function(next) {
+  var user = this;
+  if (!user.isModified('password')) return next();
+  bcrypt.hash(user.password, 14).then(
+    hashedPassword => {
+      user.password = hashedPassword;
+      next();
+    },
+    err => next(err)
+  );
+});
 
-// userSchema.methods.comparePassword = function(candidatePassword, next) {
-//   bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
-//     if (err) return next(err);
-//     next(null, isMatch);
-//   });
-// };
+userSchema.methods.comparePassword = function(candidatePassword, next) {
+  bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+    if (err) return next(err);
+    next(null, isMatch);
+  });
+};
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
