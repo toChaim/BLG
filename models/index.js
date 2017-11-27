6d68e90fdef9b4a34f9f10a90ef02faff68e7fb7
toChaim <toChaim@gmail.com>
2017-11-27 14:34:03 -0800

    fix deprication warning.

--- models/index.js
+++ models/index.js
@@ -1,8 +1,15 @@
 const mongoose = require('mongoose');
-const { MONGODB_URI, MONGOOSE_DEBUUG } = require('../config');
+const { MONGODB_URI, DEVELOPMENT, ENV } = require('../config');
 
-mongoose.set('debug', MONGOOSE_DEBUUG);
-mongoose.connect(MONGODB_URI);
+mongoose.set('debug', DEVELOPMENT);
 mongoose.Promise = Promise;
+mongoose
+  .connect(MONGODB_URI, { useMongoClient: true })
+  .then(() => {
+    console.log('Database connected.');
+  })
+  .catch(err => {
+    console.log(`database failed to connected. err="${err}"`);
+  });
 
 module.exports.User = require('./user');
