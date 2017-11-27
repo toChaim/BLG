6781dc889e14f0fbf4532ea2fde4ad9e94985f12
toChaim <toChaim@gmail.com>
2017-11-27 14:34:03 -0800

    fix deprication warning.

--- config.js
+++ config.js
@@ -3,4 +3,4 @@ require('dotenv').load();
 exports.SECRET_KEY = process.env.SECRET_KEY || 'SECRET_KEY';
 exports.PORT = process.env.PORT || 3001;
 exports.MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/BLG';
-exports.MONGOOSE_DEBUUG = process.env.ENV === 'development';
+exports.DEVELOPMENT = process.env.ENV === 'development';
