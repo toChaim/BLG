dae2d2943eeddef1611d318f5924da4aadc34294
toChaim <toChaim@gmail.com>
2017-12-02 21:38:56 -0800

    fix error for login.

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,20 +1,39 @@
 const { SECRET_KEY } = require('../config');
 const jwt = require('jsonwebtoken');
 
-exports.ensureLogin = (req, res, next) => {
+exports.hasLogin = reqAuth => {
+  if (!reqAuth) {
+    return false;
+  }
   try {
-    const token =
-      req.headers.authorization && req.headers.authorization.split(' ')[1];
-    jwt.verify(token, SECRET_KEY, (err, decoded) => {
-      if (decoded) {
-        next();
-      } else {
-        var err = new Error('No Lognin');
-        err.status = 401;
-        next(err);
-      }
-    });
+    const token = reqAuth.split(' ')[1];
+    var temp = jwt.verify(token, SECRET_KEY);
+    console.log(temp);
+    return true;
   } catch (err) {
+    console.log(`*** debugger; ***`, err);
+    return false;
+  }
+};
+
+exports.ensureLogin = (req, res, next) => {
+  // const token =
+  //   req.headers.authorization && req.headers.authorization.split(' ')[1];
+  // jwt.verify(token, SECRET_KEY, (err, decoded) => {
+  //   if (decoded) {
+  //     next();
+  //   } else {
+  //     var err = new Error('No Lognin');
+  //     err.status = 401;
+  //     next(err);
+  //   }
+  // });
+  console.log('ensureLogin is not working. ');
+  if (exports.hasLogin(req.headers.authorization)) {
+    next();
+  } else {
+    var err = new Error('No Lognin');
+    err.status = 401;
     next(err);
   }
 };
