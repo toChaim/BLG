52ff34aeae95a17880eb7e25ce37c50735eb134b
toChaim <toChaim@gmail.com>
2017-12-25 17:44:20 -0800

    fix test.

---
+++ games/index.js
@@ -0,0 +1,6 @@
+// Like modles this will hold my games.
+
+module.exports = {
+  gratitueds: 'A way to share what you are grateful for.',
+  CBT: 'A game to teach and reinforce Cognitive Behavoral Therapy.'
+};
--- test/routesSpec.js
+++ test/routesSpec.js
@@ -1,24 +1,46 @@
-const request = require('supertest');
+// const request = require('supertest');
 const api = require('../api');
-const expect = require('chai').expect;
-const mongoose = require('mongoose');
-const db = require('../models/');
+// const expect = require('chai').expect;
+// const mongoose = require('mongoose');
+// const db = require('../models/');
 
-mongoose.Promise = Promise;
+// mongoose.Promise = Promise;
 
-// describe("GET /users", function() {
-//   it("responds with JSON", function(done) {
+// // describe("GET /users", function() {
+// //   it("responds with JSON", function(done) {
+// //     request(api)
+// //       .get("/userss/")
+// //       .expect("Content-Type", /json/)
+// //       .expect(200, done);
+// //   });
+// // });
+
+// describe('GET /users', function() {
+//   it('responds with JSON', function(done) {
 //     request(api)
-//       .get("/userss/")
-//       .expect("Content-Type", /json/)
+//       .get('/qqqusers')
 //       .expect(200, done);
 //   });
 // });
 
-describe('GET /users', function() {
-  it('responds with JSON', function(done) {
+const request = require('supertest');
+
+// const app = require('express')();
+
+// api.get('/users', function(req, res) {
+//   res.status(200).json({ name: 'tobi' });
+// });
+
+describe('GET /users this is running?', function() {
+  it('respond with json', function(done) {
     request(api)
-      .get('/qqqusers')
-      .expect(200, done);
+      .get('/usersaa')
+      .set('Accept', 'application/json')
+      .expect(200)
+      .end(function(err, res) {
+        //console.log('err=', err, ' res=', res);
+        if (err) return done(err);
+        done();
+      });
   });
 });
