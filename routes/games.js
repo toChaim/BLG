04af83777d2a921a589bfdd0289e793838a82ea2
toChaim <toChaim@gmail.com>
2017-12-25 17:44:06 -0800

    add games route.

--- routes/games.js
+++ routes/games.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const games = require('../games');
 
 router.get('/', (req, res) => {
   res.json({ message: 'List of Games' });
 });
 
-router.get('/:id', (req, res) => {
-  res.json({ message: 'Get a Games' });
+router.get('/:id', (req, res, next) => {
+  if (!games[req.params.id]) {
+    console.log("can't find that game.");
+    return next();
+  }
+  console.log('got a game');
+  res.json({ message: 'Get a Game' });
 });
 
 module.exports = router;
