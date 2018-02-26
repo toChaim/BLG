1d72239416c14e6aedb7d70ba7edf2c9dae5ba62
toChaim <toChaim@gmail.com>
2018-02-25 21:39:06 -0800

    first server and start script.

---
+++ api.js
@@ -0,0 +1,17 @@
+const api = require('express')();
+const PORT = process.env.PORT || 3001;
+
+//First Middleware
+api.use((req, res, next) => {
+  console.log(req);
+});
+
+//Routes
+api.use('/', (req, res, next) => {
+  res.json({ message: 'Main Page' });
+});
+
+//Start Server
+api.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
