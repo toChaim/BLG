57743321c0f5e4ce111fdeb2fb14ea1d95d430f5
Chaim Finkelman <toChaim@gmail.com>
2019-01-28 18:02:21 -0800

    clear things out

--- config.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').load();
-
-exports.SECRET_KEY = process.env.SECRET_KEY || 'SECRET_KEY';
-exports.PORT = process.env.PORT || 3001;
-exports.MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/BLG';
-exports.DEVELOPMENT = process.env.ENV === 'development';
--- routes/auth.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const { User } = require('../models');
-const { SECRET_KEY } = require('../config');
-const jwt = require('jsonwebtoken');
-const { login } = require('../middleware/auth');
-
-router.post('/login', login);
-
-module.exports = router;
