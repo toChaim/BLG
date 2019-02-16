4a183e7b5e6a567480cb3aef74d4383f91ca99d6
Chaim Finkelman <toChaim@gmail.com>
2019-02-15 20:59:56 -0800

    counter working

--- src/App.js
+++ src/App.js
@@ -4,6 +4,21 @@ import './App.css';
 import TimerDisplay from './components/TimerDisplay';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      time: new Date()
+    };
+    this.handleChange.bind(this);
+    this.intervalHandler = setInterval(this.handleChange.bind(this), 1000);
+  }
+
+  handleChange(event) {
+    this.setState({
+      time: new Date()
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,7 +26,7 @@ class App extends Component {
           <p>
             Better Living Games
           </p>
-          <TimerDisplay time={new Date()}/>
+          <TimerDisplay time={this.state.time}/>
         </header>
       </div>
     );
