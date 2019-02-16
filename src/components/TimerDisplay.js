b5604721789dd47c8a8969f819cd4f1183982fae
Chaim Finkelman <toChaim@gmail.com>
2019-02-16 07:46:03 -0800

    deal with negative times

--- src/components/TimerDisplay.js
+++ src/components/TimerDisplay.js
@@ -3,10 +3,13 @@ import React from 'react';
 const leadZeor = num => Math.floor(num) > 9? `${Math.floor(num)}`:`0${Math.floor(num)}`; 
 
 export default ({time}) =>{
-  const sec = time/1000 - time.getTimezoneOffset()*60;
+  // track sign for negative numbers
+  const sign = time < 0? -1 : 1;
+  // convert to local seconds and make a positive number
+  const sec = (time/1000 - time.getTimezoneOffset()*60) * sign;
 
   return (<div className="TimerDisplay">
-    {Math.floor(sec/60/60%24)}
+    {Math.floor(sign*sec/60/60%24)}
     :{leadZeor(sec/60%60)}
     :{leadZeor(sec%60)}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
   </div>);
