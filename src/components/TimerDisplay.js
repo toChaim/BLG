5a1b2102f92b0ac166bf182c28f4a8430a29b45d
Chaim Finkelman <toChaim@gmail.com>
2019-02-15 18:16:14 -0800

    fix display time

--- src/components/TimerDisplay.js
+++ src/components/TimerDisplay.js
@@ -3,7 +3,7 @@ import React from 'react';
 const leadZeor = num => Math.floor(num) > 9? `${Math.floor(num)}`:`0${Math.floor(num)}`; 
 
 export default ({time}) =>{
-  const sec = time/1000 + time.getTimezoneOffset()*60;
+  const sec = time/1000 - time.getTimezoneOffset()*60;
 
   return (<div className="TimerDisplay">
     {Math.floor(sec/60/60%24)}
