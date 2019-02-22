5cdd111f80da4c0e1530b9ed64903955a9f872f6
Chaim Finkelman <toChaim@gmail.com>
2019-02-21 17:50:07 -0800

    fix negative numbers

--- src/components/TimerDisplay.js
+++ src/components/TimerDisplay.js
@@ -2,16 +2,13 @@ import React from 'react';
 
 const leadZeor = num => Math.floor(num) > 9? `${Math.floor(num)}`:`0${Math.floor(num)}`; 
 
-export default ({time = 0, local = true}) =>{
-  // track sign for negative numbers
-  const sign = time < 0? -1 : 1;
-  // convert to time to local
-  const sec = time - (local ? new Date().getTimezoneOffset()*60 : 0);
-
+export default ({time = 0}) =>{
+  const sec = Math.abs(time);
 
   return (<div className="TimerDisplay">
-    {Math.floor(sec/60/60%24)}
-    :{leadZeor(sign*sec/60%60)}
-    :{leadZeor(sign*sec%60)}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
+    {time < 0? '-' : ''}
+    {Math.floor(sec/60/60%1000)}
+    :{leadZeor(sec/60%60)}
+    :{leadZeor(sec%60)}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
   </div>);
 };
--- src/components/TimerDisplay.test.jsx
+++ src/components/TimerDisplay.test.jsx
@@ -8,27 +8,56 @@ import { mount } from 'enzyme';
 import TimerDisplay from './TimerDisplay';
 
 describe('TimerDisplay', () => {
-  let props;
-  let mountedTimerDisplay;
-  const timerDisplay = () => {
-    if (!mountedTimerDisplay) {
-      mountedTimerDisplay = mount(
-        <TimerDisplay {...props} />
-      );
-    }
-    return mountedTimerDisplay;
-  };
 
-  beforeEach(() => {
-    props = {
-      time: undefined,
-      local: undefined,
-    };
-    mountedTimerDisplay = undefined;
+  it('renders a div', () => {
+    const divs = mount(<TimerDisplay />).find('div');
+    expect(divs.length).toBeGreaterThan(0);
   });
 
-  it('always renders a div', () => {
-    const divs = timerDisplay().find('div');
-    expect(divs.length).toBeGreaterThan(0);
+  describe('renders correct times', () => {
+    it('no props', () => {
+      const text = mount(<TimerDisplay />).text();
+      expect(text).toBe('0:00:00');
+    });
+    it('1', () => {
+      const text = mount(<TimerDisplay {...{ time: 1 }} />).text();
+      expect(text).toBe('0:00:01');
+    });
+    it('-1', () => {
+      const text = mount(<TimerDisplay {...{ time: -1 }} />).text();
+      expect(text).toBe('-0:00:01');
+    });
+    it('12', () => {
+      const text = mount(<TimerDisplay {...{ time: 12 }} />).text();
+      expect(text).toBe('0:00:12');
+    });
+    it('123', () => {
+      const text = mount(<TimerDisplay {...{ time: 113 }} />).text();
+      expect(text).toBe('0:01:53');
+    });
+    it('200', () => {
+      const text = mount(<TimerDisplay {...{ time: 200 }} />).text();
+      expect(text).toBe('0:03:20');
+    });
+    it('20000', () => {
+      const text = mount(<TimerDisplay {...{ time: 2000 }} />).text();
+      expect(text).toBe('0:33:20');
+    });
+    it('200000', () => {
+      const text = mount(<TimerDisplay {...{ time: 20000 }} />).text();
+      expect(text).toBe('5:33:20');
+    });
+    it('200000', () => {
+      const text = mount(<TimerDisplay {...{ time: 200000 }} />).text();
+      expect(text).toBe('55:33:20');
+    });
+    it('2000000', () => {
+      const text = mount(<TimerDisplay {...{ time: 2000000 }} />).text();
+      expect(text).toBe('555:33:20');
+    });
+    it('-2000000', () => {
+      const text = mount(<TimerDisplay {...{ time: -2000000 }} />).text();
+      expect(text).toBe('-555:33:20');
+    });
   });
 });
