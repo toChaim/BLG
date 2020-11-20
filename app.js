f1ad8278bdd391123d73091ce1ca88f84c7a9408
Chaim Finkelman <toChaim@gmail.com>
2020-11-19 18:20:28 -0800

    add first routes

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("BLG"));
+var Users = {
+  chaim: {
+    email: 'toChaim@gmail.com',
+    passhash: 'xxxxxxx12345',
+    privacy: 'public',
+    friends: [],
+  }
+}
 
+// home
+app.get("/", (req, res) => res.send("BLG"));
+// prayer
+// gratbot
+// CBT
+// users
+app.get("/users/:username", (req, res) => {
+  res.json(Users[req.params.username]);
+});
+app.get("/users", (req, res) => {
+  res.send(Object.keys(Users));
+});
+app.post("/users/", (req, res) => {
+
+})
 app.listen(3000, () => console.log("app listening on prot 3000"));
