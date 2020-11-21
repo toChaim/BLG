be06815a258ddd48eaafbd0aca83e5a18d589470
Chaim Finkelman <toChaim@gmail.com>
2020-11-21 00:27:52 -0800

    add snapshot test

--- __tests__/app.js
+++ __tests__/app.js
@@ -1,9 +1,13 @@
 const request = require('supertest');
 const app = require('../app');
 
-describe('GET / ', () => {
-  test('It should respond with an array of students', async () => {
-    const response = await request(app).get('/');
-    expect(response.statusCode).toBe(200);
+describe('Test App', () => {
+  describe('GET / ', () => {
+    test('Should return home page', async () => {
+      const res = await request(app).get('/');
+      expect(res.statusCode).toBe(200);
+      expect(res.type).toBe('text/html');
+      expect(res.body).toMatchSnapshot();
+    });
   });
 });
