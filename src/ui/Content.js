c10466da34c028fbd41cfff2d373265dd8aac97a
Chaim Finkelman <toChaim@gmail.com>
2022-04-28 11:03:38 -0400

    fix prod content call

--- src/ui/Content.js
+++ src/ui/Content.js
@@ -2,15 +2,14 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const { NODE_ENV, PORT = 5000 } = process.env;
-console.log(process.env);
-const url = NODE_ENV ? 'https://better-living-games.herokuapp.com' : `http://localhost:${PORT}/api`;
+const url = NODE_ENV ? 'https://better-living-games.herokuapp.com/api' : `http://localhost:${PORT}/api`;
 
 function Content() {
   const [content, setContent] = useState('loading content');
   useEffect(() => {
     axios.get(url)
       .then(res => {
-        setContent(res.data?.data?.message);
+        setContent(res.data?.data?.message ?? 'failed to load content');
       })
       .catch(err => {
         console.log(err);
