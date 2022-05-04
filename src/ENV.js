b8ac603c01b7b5abb60eb6f037bb80ec5a17e85e
Chaim Finkelman <toChaim@gmail.com>
2022-05-04 17:06:51 -0400

    fix start methods

--- src/ENV.js
+++ src/ENV.js
@@ -1,7 +1,7 @@
-const { PRODUCTION_HOST, PRODUCTION } = require('./CONSTANTS');
+const { LOCALHOST, PRODUCTION_HOST, PRODUCTION } = require('./CONSTANTS');
 
 const ENV = Object.assign({
-  HOST: process.env.REACT_APP_HOST || PRODUCTION_HOST,
+  HOST: process.env.PORT ? PRODUCTION_HOST : LOCALHOST,
   PORT: 5000,
   NODE_ENV: process.env.REACT_APP_NODE_ENV || PRODUCTION,
 }, process.env);
